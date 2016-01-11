79c984577f60abf4b04feb52d1c9e4068219ad34
Abigail Rolling and Wade Grubbs <arolling@gmail.com>
2016-01-11 14:01:56 -0800

    make expandable text web page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 jQuery(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").toggle();
-    $("#initially-hidden").toggle();
+    $("#initially-showing").slideToggle();
+    $("#initially-hidden").slideToggle();
+  });
+
+  $("#clickable-1").click(function() {
+    $("#initially-hidden-1").fadeToggle();
+  });
+
+  $("#clickable-2").click(function() {
+    $("#initially-hidden-2").fadeToggle();
+  });
+
+  $("#clickable-3").click(function() {
+    $("#initially-hidden-3").fadeToggle();
   });
 });
